07d5f2c2c92262ec4f9a94fb77e447b17842a0a5
Manoj Thavidaboena <tmanoj1904@gmail.com>
2023-09-18 10:34:00 +0530

    first commit

---
+++ src/test/java/com/tripleightsolutions/casemanagement/CaseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripleightsolutions.casemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
